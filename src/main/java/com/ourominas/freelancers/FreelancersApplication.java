ffaf4836b10e394ae1fb12b7e33978900a3b92d7
Pedro Basilio <phbasilio6903@gmail.com>
2025-04-08 09:40:51 -0300

    feat: initial commit

---
+++ src/main/java/com/ourominas/freelancers/FreelancersApplication.java
@@ -0,0 +1,13 @@
+package com.ourominas.freelancers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ourominas/freelancers/FreelancersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourominas.freelancers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
