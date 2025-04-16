913aca2c10bf76665a79305e526c18c4cc70d055
Pedro Basilio <phbasilio6903@gmail.com>
2025-04-16 09:38:47 -0300

    feat: adicionando os getters e setters com o lombok

--- src/main/java/com/ourominas/freelancers/domain/Extra.java
+++ src/main/java/com/ourominas/freelancers/domain/Extra.java
@@ -6,11 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.UUID;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "extras")
