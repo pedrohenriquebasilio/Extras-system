6ffadc6a07e954aff652d091ab6a720b3e9b9891
Pedro Basilio <phbasilio6903@gmail.com>
2025-04-16 09:39:46 -0300

    feat: adicionando o metodo atualizarUsuario do Extras

--- src/main/java/com/ourominas/freelancers/services/extraservices/Extraservices.java
+++ src/main/java/com/ourominas/freelancers/services/extraservices/Extraservices.java
@@ -2,6 +2,11 @@ package com.ourominas.freelancers.services.extraservices;
 
 
 import com.ourominas.freelancers.domain.Extra;
+import com.ourominas.freelancers.domain.Users;
+import com.ourominas.freelancers.domain.dto.request.ExtraRequestDTO;
+import com.ourominas.freelancers.domain.dto.request.UserRequestDTO;
+import com.ourominas.freelancers.domain.dto.response.ExtraResponseDTO;
+import com.ourominas.freelancers.domain.dto.response.UserResponseDTO;
 import com.ourominas.freelancers.repositories.ExtraRepository;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +31,39 @@ public class Extraservices {
             throw new RuntimeException("Usuario não encontrado");
         }
         repository.deleteById(id);
+    }
+
+    public ExtraResponseDTO atualizarUsuario(UUID id, ExtraRequestDTO dto) {
+        Extra extra = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        extra.setName(dto.name());
+        extra.setEmail(dto.email());
+        extra.setRg(dto.rg());
+        extra.setPis(dto.pis());
+        extra.setSefip(dto.Sefip());
+        extra.setESocial(dto.esocial());
+        extra.setTelefone(dto.telefone());
+        extra.setSindicate(dto.sindicate());
+        extra.setDateBirth(dto.date_birth());
+
+
+        Extra salvo = repository.save(extra);
+
+        return new ExtraResponseDTO(
+                salvo.getId(),
+                salvo.getName(),
+                salvo.getCpf(),
+                salvo.getRg(),
+                salvo.getPis(),
+                salvo.getDateBirth(),
+                salvo.getEmail(),
+                salvo.getTelefone(),
+                salvo.getESocial(),
+                salvo.getSefip(),
+                salvo.getSindicate(),
+                salvo.isAvaliable()
+        );
     }
 
 
