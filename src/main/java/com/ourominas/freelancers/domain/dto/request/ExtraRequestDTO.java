d0b22baec552a270f6779a0cfeb76f1173beed5d
Pedro Basilio <phbasilio6903@gmail.com>
2025-04-16 09:39:23 -0300

    feat: adicionando os campos que faltavam

--- src/main/java/com/ourominas/freelancers/domain/dto/request/ExtraRequestDTO.java
+++ src/main/java/com/ourominas/freelancers/domain/dto/request/ExtraRequestDTO.java
@@ -2,6 +2,6 @@ package com.ourominas.freelancers.domain.dto.request;
 
 import java.util.Date;
 
-public record ExtraRequestDTO(Long id, String name, String cpf, String rg, String pis, Date date_birth, String email, String telefone, String esocial, String Sefip,
+public record ExtraRequestDTO( String name, String cpf, String rg, String pis, Date date_birth, String email, String telefone, String esocial, String Sefip,
                               String sindicate, boolean isAvaliable) {
 }
