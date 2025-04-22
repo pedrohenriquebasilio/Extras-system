2f6eb37dbf6b8272e69740b9f1c81f7f02ba43b7
Pedro <phbasilio6903@gmail.com>
2025-04-22 14:33:23 -0300

    feat: add methodo necessario

--- src/main/java/com/ourominas/freelancers/repositories/EventRepository.java
+++ src/main/java/com/ourominas/freelancers/repositories/EventRepository.java
@@ -5,7 +5,11 @@ import com.ourominas.freelancers.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-    long countByExtraIdAndDataInicioBetween(Long aLong, LocalDateTime inicioSemana, LocalDateTime fimSemana);
+
+
+
+    long countByExtraIdAndDataInicioBetween(UUID extraId, LocalDateTime inicioSemana, LocalDateTime fimSemana);
 }
