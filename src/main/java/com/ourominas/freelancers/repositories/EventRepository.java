166bb7588e9cba78cf9fed94909df1a56e0dd3f7
Pedro <phbasilio6903@gmail.com>
2025-04-22 13:16:33 -0300

    feat: add EventRepository

--- src/main/java/com/ourominas/freelancers/repositories/EventRepository.java
+++ src/main/java/com/ourominas/freelancers/repositories/EventRepository.java
@@ -1,9 +1,11 @@
 package com.ourominas.freelancers.repositories;
 
+
 import com.ourominas.freelancers.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
+import java.time.LocalDateTime;
 
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface EventRepository extends JpaRepository<Event, Long> {
+    long countByExtraIdAndDataInicioBetween(Long aLong, LocalDateTime inicioSemana, LocalDateTime fimSemana);
 }
